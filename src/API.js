cb95092179d05fbb6ef9a29d1e8823974b87116e
dr96mabuza <dr96mabuza@gmail.com>
2022-04-16 17:57:56 +0200

    added random gif and style

--- src/API.js
+++ src/API.js
@@ -6,6 +6,9 @@ const getWeather = () => {
   const pressure = document.querySelector("#pressure");
   const wind = document.querySelector("#wind");
   const date = document.querySelector("#date");
+  const imageContainer = document.querySelector("#imageContainer");
+  const photo = document.createElement("img");
+  imageContainer.appendChild(photo);
 
   const getTemp = (name) => {
     const nameFinal =
@@ -16,16 +19,25 @@ const getWeather = () => {
     )
       .then((response) => response.json())
       .then((response) => {
+        const picType = response.weather[0].main;
         console.log(response);
-        location.textContent = nameFinal;
-        temperature.textContent = Math.round(
-          Number(response.main.temp) - 273.15
-        );
-        description.textContent = response.weather[0].description;
-        humidity.textContent = response.main.humidity;
-        pressure.textContent = response.main.pressure;
-        wind.textContent = response.wind.speed;
-        date.textContent = new Date();
+        fetch(
+          `https://api.giphy.com/v1/gifs/translate?api_key=RIIuJYIyG5MmEDmQ3ymmwqP1p365z4uU&s=${picType}}`,
+          { mode: "cors" }
+        )
+          .then((myData) => myData.json())
+          .then((myData) => {
+            console.log(myData);
+            photo.src = myData.data.images.original.url;
+            const result = Number(response.main.temp) - 273.15;
+            location.textContent = nameFinal;
+            temperature.textContent = Math.round(result);
+            description.textContent = response.weather[0].description;
+            humidity.textContent = `humidity: ${response.main.humidity} %`;
+            pressure.textContent = `pressure: ${response.main.pressure} Pa`;
+            wind.textContent = `wind speed: ${response.wind.speed} km/h`;
+            date.textContent = new Date();
+          });
       })
       .catch((response) => {
         if (response !== Object) {
