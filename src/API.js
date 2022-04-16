6c01d39170854a722fee4432aa23744a187890a5
dr96mabuza <dr96mabuza@gmail.com>
2022-04-16 15:51:05 +0200

    added styling and display name correction

--- src/API.js
+++ src/API.js
@@ -8,15 +8,19 @@ const getWeather = () => {
   const date = document.querySelector("#date");
 
   const getTemp = (name) => {
+    const nameFinal =
+      name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=4eaaa26ad9ee84323de7fe5e30bc033a`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${nameFinal}&APPID=4eaaa26ad9ee84323de7fe5e30bc033a`,
       { mode: "cors" }
     )
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
-        location.textContent = name;
-        temperature.textContent = response.main.temp;
+        location.textContent = nameFinal;
+        temperature.textContent = Math.round(
+          Number(response.main.temp) - 273.15
+        );
         description.textContent = response.weather[0].description;
         humidity.textContent = response.main.humidity;
         pressure.textContent = response.main.pressure;
